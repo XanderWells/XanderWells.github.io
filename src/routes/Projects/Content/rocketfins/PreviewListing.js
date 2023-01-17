2a650ffee5c95cd5e10c86443efe0b2bde40913a
XanderWells <wells163@purdue.edu>
2023-01-17 10:42:01 -0500

    added rocket section

--- src/routes/Projects/Content/rocketfins/PreviewListing.js
+++ src/routes/Projects/Content/rocketfins/PreviewListing.js
@@ -1,10 +1,11 @@
 import Listing from "../../../../components/Listing/Listing";
+import fins20 from "./fins20.jpg"
 
 const PreviewListing = () => {
     const description = "I made 5 rocket fins for Purdue Space Program's Boomie-Zoomie-B Rocket (3 main and 2 spare). This took place over ~40 hours over a single weekend. It was a lot of work, but it was great fun and the results were amazing. Part of this project's success involved feeds-and-speeds testing on smaller parts in order to find the fastest possible cutting values that still yielded the desired results"   
 
     return (
-        <Listing title="Rocket Fins" description={description} />
+        <Listing title="Rocket Fins" description={description} image={fins20} />
     )
 }
 
