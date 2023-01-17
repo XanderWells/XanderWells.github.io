79f092413ec19c24f07e7c151eee6f5999ffe087
XanderWells <wells163@purdue.edu>
2023-01-17 10:41:09 -0500

    Updates

--- static/js/481.a5d997dc.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkxanderwellsmewebsite=self.webpackChunkxanderwellsmewebsite||[]).push([[481],{8481:function(e,n,s){s.r(n);var t=s(184);n.default=function(){return(0,t.jsx)("div",{children:"Rocket Fins"})}}}]);
-//# sourceMappingURL=481.a5d997dc.chunk.js.map
